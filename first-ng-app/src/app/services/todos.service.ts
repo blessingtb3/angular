c80bbe4db5feb0b85cf9912cbf724cc29793dd52
blessingtb3 <blessingtb3@gmail.com>
2025-01-27 10:21:12 +0200

    adding todos service

---
+++ first-ng-app/src/app/services/todos.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'//enabling the service to be used in the whole application
+})
+export class TodosService {
+
+  constructor() { }
+}
